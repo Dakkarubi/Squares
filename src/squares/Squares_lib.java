244ae17291c3c4f9840f0a6ba87e90b7e122aac7
reo87 <reo87@DESKTOP-7GT7ULQ>
2017-11-20 19:26:41 +0900

    theta[0]=1.25;theta[1]=4.5

--- src/squares/Squares_lib.java
+++ src/squares/Squares_lib.java
@@ -10,6 +10,7 @@ public class Squares_lib implements Squares_if {
 		this.x = x;
 		this.y = y;
 	}
+	
 	@Override
 	public double getObject() {
 		// TODO Auto-generated method stub
@@ -19,7 +20,7 @@ public class Squares_lib implements Squares_if {
 		}
 		return object / 2 ;
 	}
-
+	
 	@Override
 	public double[] getDx() {
 		// TODO Auto-generated method stub
